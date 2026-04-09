dc3def5cdafc4cfeb522d37011b723c528a1f8de
drew <drew@math.mit.edu>
2026-04-09 08:48:22 -0600

    add vpp Python programs

---
+++ vpp.py
@@ -0,0 +1,25 @@
+def pp_verify(p, A, x0):
+    from math import gcd, isqrt
+
+    if p < 5 or p % 2 == 0:
+        return False
+
+    q = isqrt(p)                            # p not prime ==> prime divisor < q
+    k = (q + 1 + isqrt(4*q)).bit_length()   # least k such that 2^k > q + 1 + 2*sqrt(q)
+
+    if gcd(A * A - 4, p) != 1:
+        return False                        # singular
+
+    X, Z = x0 % p, 1
+    Zprev = None
+
+    # precompute C = (A + 2)/4 mod p for doubling formula
+    C = ((A + 2) * ((p + 1) // 4 if p % 4 == 3 else (3*p +1) //4)) % p
+    for i in range(k):
+        Zprev = Z
+        U,V = (X+Z)*(X+Z) % p, (X-Z)*(X-Z) % p
+        W = U - V
+        X,Z = U*V % p, W*(V+C*W) % p
+
+    # if p|Z and (Zprev,p)=1 then (p,A,x0) is valid
+    return Z % p == 0 and gcd(Zprev, p) == 1 
